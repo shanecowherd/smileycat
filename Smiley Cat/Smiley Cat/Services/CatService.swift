3a6d9c6ed4e9b1db4bfc1e2c1bf4b255e411e646
Shane Cowherd <shane@cowherd.com>
2022-11-28 16:09:01 -0700

    Added Services

--- Smiley Cat/Smiley Cat/Services/CatService.swift
+++ Smiley Cat/Smiley Cat/Services/CatService.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import Realm
+import Realm.Private
+import RealmSwift
+
+@MainActor
+class CatService: ObservableObject {
+    let realm = try? Realm()
+    
+    init(skipDownloading: Bool = false) {
+        
+        /// Allow CatService to be initialized without downloading.
+        guard !skipDownloading else {
+            return
+        }
+        
+        Task {
+            do {
+                try await download()
+            } catch {
+                debugPrint("Consider showing an alert popup, currently placeholder cats will be displayed.")
+            }
+        }
+    }
+    
+    func download() async throws {
+        
+        /// Cataas.com API to grab the meta data for 16 gifs.
+        let cataasApiUrl = URL(string: "https://cataas.com/api/cats?limit=16&skip=0&tags=gif")!
+        
+        /// Download the metadata for 16 gifs.
+        let json = try await URLSession.shared.data(from: cataasApiUrl)
+        let downloadedCats = try JSONDecoder().decode([CatNetworkResponse].self, from: json.0)
+        
+        /// Get all downloaded cats from Realm.
+        let existingCats = all()
+        
+        /// Loop over all the downloaded cats, if we find a new cat, go ahead and add it to Realm.
+        for cat in downloadedCats {
+            let existingCat = existingCats.first { $0.sourceId == cat.id }
+            
+            if existingCat == nil {
+                realm?.writeAsync({
+                    let sourceUrl = "https://cataas.com/cat/\(cat.id)"
+                    let newCat = Cat(sourceId: cat.id, sourceUrl: sourceUrl)
+                    
+                    debugPrint("🟢 Adding item to realm: \(cat.id)")
+                    self.realm?.add(newCat)
+                })
+            }
+        }
+    }
+    
+    func all() -> Results<Cat> {
+        let realm = try! Realm()
+        return realm.objects(Cat.self)
+    }
+}
--- Smiley Cat/Smiley Cat/Services/SoundService.swift
+++ Smiley Cat/Smiley Cat/Services/SoundService.swift
@@ -5,4 +5,22 @@
 //  Created by Shane Cowherd on 11/27/22.
 //
 
-import Foundation
+import AVFoundation
+
+ class SoundService {
+
+   static var player: AVAudioPlayer?
+
+   static func playSounds(soundfile: String) {
+
+       if let path = Bundle.main.path(forResource: soundfile, ofType: nil) {
+           do {
+               player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+               player?.prepareToPlay()
+               player?.play()
+           } catch {
+               debugPrint("Unable to play audio")
+           }
+       }
+    }
+ }
